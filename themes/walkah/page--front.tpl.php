6b2f2213cd8ed2e174cdd81ec2810630d383d59c
James Walker <walkah@walkah.net>
2011-02-28 17:26:32 -0500

    flickr for now

--- themes/walkah/page--front.tpl.php
+++ themes/walkah/page--front.tpl.php
@@ -23,7 +23,7 @@
     <img src="<?php print $logo; ?>" class="photo" alt="James Walker" style="float: right; margin: 0 0 1em 1em; padding: 5px; border: 1px solid #676767; vertical-align:top" />
     <p>Hey, I'm James Walker (a.k.a. "walkah").</p>
     <p>I am a developer and believe in open technology.</p>
-    <p>I have a <a href="/blog">blog</a> and sometimes take <a href="http://gallery.walkah.net/">pictures</a>.</p>
+    <p>I have a <a href="/blog">blog</a> and sometimes take <a href="http://flickr.com/walkah">pictures</a>.</p>
     <p>You should totally <a href="/contact">contact me</a>.</p>
   </div>
 </div> <!-- /#page -->
