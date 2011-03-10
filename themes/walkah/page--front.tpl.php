f2bded7263d6927e04e2add9d6aa3c617d0f67de
James Walker <walkah@walkah.net>
2011-03-10 10:54:21 -0600

    elsewhere icons

--- themes/walkah/page--front.tpl.php
+++ themes/walkah/page--front.tpl.php
@@ -1,5 +1,4 @@
 <div id="page">
-  
   <header role="banner" class="clearfix">
     <?php if ($site_name || $site_slogan): ?>
     <hgroup id="name-and-slogan">
@@ -21,9 +20,16 @@
   
   <div id="front" class="clearfix vcard">
     <img src="<?php print $logo; ?>" class="photo" alt="James Walker" style="float: right; margin: 0 0 1em 1em; padding: 5px; border: 1px solid #676767; vertical-align:top" />
-    <p>Hey, I'm James Walker (a.k.a. "walkah").</p>
+    <p>Hey, I'm <span class="fn">James Walker</span> (a.k.a. <a href="http://walkah.net/" rel="me" class="url uid">walkah</a>).</p>
     <p>I am a developer and believe in open technology.</p>
-    <p>I have a <a href="/blog">blog</a> and sometimes take <a href="http://flickr.com/walkah">pictures</a>.</p>
+    <p>I have a <a href="/blog">blog</a> and sometimes take <a href="http://flickr.com/walkah" rel="me">pictures</a>.</p>
     <p>You should totally <a href="/contact">contact me</a>.</p>
+    <p class="elsewhere">
+      <a href="http://twitter.com/walkah" rel="me"><img src="<?php print $directory ?>/images/twitter.png" class="" alt="twitter profile" /></a>
+      <a href="https://github.com/walkah" rel="me"><img src="<?php print $directory ?>/images/github.png" class="" alt="github profile" /></a>
+      <a href="http://flickr.com/walkah" rel="me"><img src="<?php print $directory ?>/images/flickr.png" class="" alt="flickr profile" /></a>
+      <a href="http://drupal.org/user/1531" rel="me"><img src="<?php print $directory ?>/images/drupal.png" class="" alt="drupal.org" /></a>
+      <a href="http://walkah.net/node/feed"><img src="<?php print $directory ?>/images/rss.png" class="" alt="rss feed" /></a>
+    </p>
   </div>
 </div> <!-- /#page -->
