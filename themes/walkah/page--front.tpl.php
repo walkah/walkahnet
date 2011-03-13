a9ab1bb42af121e504f1146916e9e1bc48be8cde
James Walker <walkah@walkah.net>
2011-03-13 14:38:37 -0400

    wording cleanup

--- themes/walkah/page--front.tpl.php
+++ themes/walkah/page--front.tpl.php
@@ -18,12 +18,12 @@
     
   </header> <!-- /header -->
   
-  <div id="front" class="clearfix vcard">
-    <img src="<?php print $logo; ?>" class="photo" alt="James Walker" style="float: right; margin: 0 0 1em 1em; padding: 5px; border: 1px solid #676767; vertical-align:top" />
-    <p>Hey, I'm <span class="fn">James Walker</span> (a.k.a. <a href="http://walkah.net/" rel="me" class="url uid">walkah</a>).</p>
-    <p>I am a developer and believe in open technology.</p>
-    <p>I have a <a href="/blog">blog</a> and sometimes take <a href="http://flickr.com/walkah" rel="me">pictures</a>.</p>
-    <p>You should totally <a href="/contact">contact me</a>.</p>
+  <div id="front" class="clearfix vcard" typeof="foaf:Person" about="#me">
+    <img src="<?php print $logo; ?>" rel="foaf:img" class="photo" alt="James Walker" style="float: right; margin: 0 0 1em 1em; padding: 5px; border: 1px solid #676767; vertical-align:top" />
+    <p>Hey, I'm <span class="fn n" property="foaf:name">James Walker</span> (a.k.a. <a href="http://walkah.net/" rel="me" property="foaf:nickname" class="url nickname uid">walkah</a>).</p>
+    <p>I am a developer and believe in the <span property="foaf:interest">open web</span>.</p>
+    <p>Find out <a href="/james-walker">more about me</a> or read my <a href="/blog">blog</a>.</p>
+    <p>While you're here, <a href="/contact">contact me</a> too.</p>
     <p class="elsewhere">
       <a href="http://twitter.com/walkah" rel="me"><img src="<?php print $directory ?>/images/twitter.png" class="" alt="twitter profile" /></a>
       <a href="https://github.com/walkah" rel="me"><img src="<?php print $directory ?>/images/github.png" class="" alt="github profile" /></a>
